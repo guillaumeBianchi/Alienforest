038a6102605cb9057f745c56586bb624d726743b
guillaumeBianchi <g.bianchi@hotmail.fr>
2016-11-02 11:51:50 +0100

    PlayerMotor + Controller: Tuto brackeys => moving right/left, back/forward

---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+//Dependency: A PlayerMotor component is attached to the gameobject w/e.
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+	[SerializeField]
+	private float speed = 10f;
+
+	private PlayerMotor motor;
+
+	void Start()
+	{
+		motor = GetComponent<PlayerMotor>();
+	}
+
+	void Update()
+	{
+		//Get inputs and calculate movement velocity as a 3D vector.
+		float x_mov = Input.GetAxisRaw("Horizontal");
+		float z_mov = Input.GetAxisRaw("Vertical");
+
+		//transform.right instead of Vector3.right because this one is local, we want the current rotation.
+		Vector3 move_horizontal = transform.right * x_mov;
+		Vector3 move_vertical = transform.forward * z_mov;
+
+		// .normalized makes the length of the vector equal to one. (move_vertical + move_horizontal) > 1 because we add both. But we only want them to give us the direction.
+		Vector3 velocity = (move_vertical + move_horizontal).normalized * speed;
+
+		motor.Move (velocity);
+	}
+}
---
+++ Scripts/PlayerMotor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+	
+	private Vector3 velocity = Vector3.zero;
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	//Gets a movement vector.
+	public void Move(Vector3 _velocity)
+	{
+		velocity = _velocity;
+	}
+
+	//Runs every physics interaction
+	void FixedUpdate()
+	{
+		PerformMovement();
+	}
+
+	void PerformMovement()
+	{
+		if (velocity != Vector3.zero) 
+		{
+			//Better than transform.translate because it stops if there is a collision.
+			rb.MovePosition (rb.position + velocity * Time.fixedDeltaTime);
+		}
+	}
+}
